130ed95c3a2b086f0177e282cc1637f559b428c8
Wilson Tran <Wilson.tran@uwaterloo.ca>
2014-03-11 23:55:12 -0400

    animation

--- llama2/src/com/cjw/llama2/MainActivity.java
+++ llama2/src/com/cjw/llama2/MainActivity.java
@@ -1,20 +1,35 @@
 package com.cjw.llama2;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout.LayoutParams;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	ImageView imageView;
+	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    	
-        final View mainView  = (View) findViewById(R.id.RelativeLayout1);
+        
+        imageView = (ImageView) findViewById(R.id.imageView1);
+        
+        final TranslateAnimation upAnimation = new TranslateAnimation(0, 0, 0, -100);
+        upAnimation.setDuration(1000);
+        upAnimation.setFillAfter(false);
+        upAnimation.setAnimationListener(new MyAnimationListener());
+        
+        final View mainView  = findViewById(R.id.RelativeLayout1);
         mainView.setOnTouchListener(new OnSwipeTouchListener(this.getApplicationContext()) {
             public void onSwipeTop() {
+            	imageView.startAnimation(upAnimation);
                 Toast.makeText(MainActivity.this, "top", Toast.LENGTH_SHORT).show();
             }
             public void onSwipeRight() {
@@ -27,5 +42,25 @@ public class MainActivity extends Activity {
                 Toast.makeText(MainActivity.this, "bottom", Toast.LENGTH_SHORT).show();
             }
         });
-    }               
+    }           
+	
+	private class MyAnimationListener implements AnimationListener{
+
+	    @Override
+	    public void onAnimationEnd(Animation animation) {
+	        imageView.clearAnimation();
+	        LayoutParams lp = new LayoutParams(imageView.getWidth(), imageView.getHeight());
+	        lp.setMargins(imageView.getLeft(), imageView.getTop()-100, 0, 0);
+	        imageView.setLayoutParams(lp);
+	    }
+
+	    @Override
+	    public void onAnimationRepeat(Animation animation) {
+	    }
+
+	    @Override
+	    public void onAnimationStart(Animation animation) {
+	    }
+
+	}
 }
